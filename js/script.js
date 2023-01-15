31863ad5aa4e7f61189783a4ca8885fc4a24755d
Abner Tarso <abnertarsolima@gmail.com>
2023-01-14 23:15:44 -0300

    att

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 import fetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -27,8 +27,9 @@ tooltip.init();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
 initAnimacaoScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
